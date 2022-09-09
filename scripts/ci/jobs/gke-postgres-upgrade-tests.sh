afa37eb132476b6eadfdb8267e486d46ea4aa01a
Dave Shrewsberry <dashrews@redhat.com>
2022-09-09 12:58:53 -0400

    experiment

--- scripts/ci/jobs/gke-postgres-upgrade-tests.sh
+++ scripts/ci/jobs/gke-postgres-upgrade-tests.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
 echo "A stub for a yet to be implemented test"
+
+echo "Trying something to get moving while figuring out the OSCI path forward"
+
+python3 ./gke_postgres_upgrade_tests.py
---
+++ scripts/ci/jobs/gke_postgres_upgrade_tests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run the upgrade test in a GKE cluster
+"""
+import os
+from runners import ClusterTestRunner
+from clusters import GKECluster
+from pre_tests import PreSystemTests
+from ci_tests import UpgradeTest
+from post_tests import PostClusterTest, FinalPost
+
+# set required test parameters
+os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
+
+# use postgres
+os.environ["ROX_POSTGRES_DATASTORE"] = "true"
+
+print("Lets see if this actually works")
+
+ClusterTestRunner(
+    cluster=GKECluster("upgrade-test"),
+    pre_test=PreSystemTests(),
+    test=UpgradeTest(),
+    post_test=PostClusterTest(),
+    final_post=FinalPost(
+        store_qa_test_debug_logs=True,
+        store_qa_spock_results=True,
+    ),
+).run()
