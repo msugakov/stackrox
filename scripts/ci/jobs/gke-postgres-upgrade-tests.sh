27999a9a070de94e1aff27921285a0fc52136c21
Dave Shrewsberry <dashrews@redhat.com>
2022-09-09 14:41:44 -0400

    more info

--- scripts/ci/jobs/gke-postgres-upgrade-tests.sh
+++ scripts/ci/jobs/gke-postgres-upgrade-tests.sh
@@ -4,4 +4,6 @@ echo "A stub for a yet to be implemented test"
 
 echo "Trying something to get moving while figuring out the OSCI path forward"
 
+echo $(pwd)
+
 python3 ./gke_postgres_upgrade_tests.py
