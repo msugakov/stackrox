b442c0b7b6b1ae346f34629a277748b8f53aa75b
Dave Shrewsberry <dashrews@redhat.com>
2022-09-09 16:04:52 -0400

    more info

--- scripts/ci/jobs/gke-postgres-upgrade-tests.sh
+++ scripts/ci/jobs/gke-postgres-upgrade-tests.sh
@@ -6,4 +6,4 @@ echo "Trying something to get moving while figuring out the OSCI path forward"
 
 echo $(pwd)
 
-python3 ./gke_postgres_upgrade_tests.py
+python3 ./scripts/ci/jobs/gke_postgres_upgrade_tests.py
