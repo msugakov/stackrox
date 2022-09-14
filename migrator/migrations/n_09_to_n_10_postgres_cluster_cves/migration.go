c5e065a15192760c91a9bacfe078d9c5bc6217d7
Dave Shrewsberry <dashrews@redhat.com>
2022-09-14 06:53:45 -0400

    cleanup

--- migrator/migrations/n_09_to_n_10_postgres_cluster_cves/migration.go
+++ migrator/migrations/n_09_to_n_10_postgres_cluster_cves/migration.go
@@ -4,7 +4,6 @@ package n9ton10
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
@@ -44,13 +43,8 @@ func move(gormDB *gorm.DB, postgresDB *pgxpool.Pool, legacyStore legacy.Store) e
 	ctx := sac.WithAllAccess(context.Background())
 	store := pgStore.New(postgresDB)
 	pkgSchema.ApplySchemaForTable(context.Background(), gormDB, schema.Table)
-	_, err := postgresDB.Exec(ctx, fmt.Sprintf("ALTER TABLE %s DISABLE TRIGGER ALL", schema.Table))
-	if err != nil {
-		log.WriteToStderrf("failed to disable triggers for %s", schema.Table)
-		return err
-	}
 	var clusterCves []*storage.ClusterCVE
-	err = walk(ctx, legacyStore, func(obj *storage.CVE) error {
+	err := walk(ctx, legacyStore, func(obj *storage.CVE) error {
 		clusterCves = append(clusterCves, convertCVEToClusterCVEs(obj)...)
 		if len(clusterCves) == batchSize {
 			if err := store.UpsertMany(ctx, clusterCves); err != nil {
@@ -69,11 +63,6 @@ func move(gormDB *gorm.DB, postgresDB *pgxpool.Pool, legacyStore legacy.Store) e
 			log.WriteToStderrf("failed to persist cluster_cves to store %v", err)
 			return err
 		}
-	}
-	_, err = postgresDB.Exec(ctx, fmt.Sprintf("ALTER TABLE %s ENABLE TRIGGER ALL", schema.Table))
-	if err != nil {
-		log.WriteToStderrf("failed to enable triggers for %s", schema.Table)
-		return err
 	}
 	return nil
 }
